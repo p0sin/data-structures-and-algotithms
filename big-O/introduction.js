e47121025c2a924d9bfe2528cac19230cd9b0671
p0sin <elposin@gmail.com>
2024-04-09 08:23:53 -0600

    firts commit

---
+++ big-O/introduction.js
@@ -0,0 +1,7 @@
+const nemo = ['nemo'];
+
+function findNemo(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log('Found NEMO')
+    }
+}
---
+++ big-O/traduction.py
@@ -0,0 +1,7 @@
+nemo = ['nemo']
+
+def findNemo(array):
+    for i in range(len(array)):
+        print('Found NEMO')
+
+findNemo(nemo)
