27c7b75cae24343bb90e999487eda98c7e495057
p0sin <elposin@gmail.com>
2024-04-09 12:46:19 -0600

    introducion and google interview code

--- big-O/traduction.py
+++
@@ -1,7 +0,0 @@
-nemo = ['nemo']
-
-def findNemo(array):
-    for i in range(len(array)):
-        print('Found NEMO')
-
-findNemo(nemo)
--- big-o/introduction/introduction.js
+++ big-o/introduction/introduction.js
@@ -1,7 +1,10 @@
 const nemo = ['nemo'];
 
 function findNemo(array) {
+    let t0 = performance.now()
     for (let i = 0; i < array.length; i++) {
         console.log('Found NEMO')
     }
+    let t1 = performance.now();
+    console.log('Call to find Nemo took' + (t1-t0 + 'milliseconds'));
 }
